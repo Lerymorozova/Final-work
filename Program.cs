09b19bfaf04a94f77f854a66465aa947a121c564
valerya <reykon-kun@yandex.ru>
2023-01-28 21:17:54 +0200

    решение задачи

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array;
+Console.WriteLine("Введите массив строк через пробел");
+string a = Console.ReadLine();
+array = a.Split(' ');
+var result = new string[array.Length];
+var Size = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[Size] = value;
+        Size++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, Size));
+Console.WriteLine();
